f31e8b8dff3513c1364e270f85c3758042f574d0
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-03 15:53:24 +0200

    third commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+// let selectSide = document.querySelector('.header__side');
+// let currentLang = document.querySelector('.header__lang');
+// let listLang = document.querySelector('.header__list--lang');
+// let imgLang = document.querySelector('.header__lang--img');
+
+// selectSide.addEventListener('click' , ()=>{
+//   currentLang.classList.toggle('.header__list--lang--active');
+//   console.log(currentLang)
+// });
