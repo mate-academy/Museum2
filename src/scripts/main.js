213429cf3d4893b4690ce0553a4eb6493394fcf7
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-04 15:00:16 +0200

    menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,13 @@
 //   currentLang.classList.toggle('.header__list--lang--active');
 //   console.log(currentLang)
 // });
+(() => {
+  const buttonLine = document.querySelector('.header__line');
+  const art = document.querySelector('.art');
+  const nav = document.querySelector('.nav');
+
+  buttonLine.addEventListener('click', () => {
+    art.classList.toggle('art__close');
+    nav.classList.toggle('nav__open');
+  });
+})();
