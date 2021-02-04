498122d14a09b58dd24d87fd9edc0129c6156692
morozoleh <olegmoroz93@gmail.com>
2021-02-04 13:53:00 +0200

    js blur

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -8,28 +8,14 @@
     dropDown.classList.toggle('dropdown--active');
   });
 
-  langBtnRef.addEventListener('focus', () => {
-    dropDown.classList.toggle('dropdown--active');
+  langBtnRef.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      dropDown.classList.toggle('dropdown--active');
+    }
   });
 
   langBtnRef.addEventListener('blur', () => {
     dropDown.classList.toggle('dropdown--active');
-  });
+  }
+  );
 })();
-
-// (() => {
-//   const menuBtnRef = document.querySelector('[data-menu-button]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
-//   const mobileDropRef = document.querySelector('[data-lang]');
-
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded
-//             = menuBtnRef.getAttribute('aria-expanded') === 'true' || 'false';
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//     mobileDropRef.classList.toggle('is-open');
-//   });
-// })();
