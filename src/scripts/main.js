4c1259adcb75518d18d92bd8fb1ba1a4a71beac8
Julia <mjulia1794@gmail.com>
2021-02-25 22:57:19 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// function toggleMenu(){
+//     var element = document.getElementsByClassName('header__menu')[0];
+
+//     if(element.style.display === 'none'){
+//         element.style.display = 'flex';
+//     }
+//     else{
+//         element.style.display = 'none';
+//     }
+// }
