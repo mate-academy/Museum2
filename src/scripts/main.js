ae0d7ee0e283070ad8a167a0ab174e441df45435
morozoleh <olegmoroz93@gmail.com>
2021-03-19 18:15:01 +0200

    fixed-bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const submit = document.querySelector('.follow__button');
+const input = document.querySelector('.follow__input');
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  input.value = '';
+});
