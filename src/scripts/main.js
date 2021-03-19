a87d5fb45899cd583667045c183cf3c0265f5daa
morozoleh <olegmoroz93@gmail.com>
2021-03-19 18:42:50 +0200

    forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,5 @@ const submit = document.querySelector('.follow__button');
 const input = document.querySelector('.follow__input');
 
 submit.addEventListener('click', (event) => {
-  event.preventDefault();
-  input.value = '';
+  setTimeout(() => (input.value = ''), 0);
 });
